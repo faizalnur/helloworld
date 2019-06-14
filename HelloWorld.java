68a0f81354844e1bc01a2f28c6b53bbc9e1f5bc3
horro <faizalfachrozi@gmail.com>
2019-06-14 10:47:59 +0700

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
